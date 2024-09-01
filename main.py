ee6aacb6e16ebcdd418f2100a2e9dfa9983a4e57
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-01 16:55:04 -0300

    feat: add game constants and main function

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()
