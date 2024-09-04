d752475b02b8d7af321fdcdfad4d57204bfb3bbb
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-04 09:08:36 -0300

    feat: add game loop, also game closes correctly

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
+    if SCREEN_WIDTH is None:
+        raise ValueError("SCREEN_WIDTH is None")
+    if SCREEN_HEIGHT is None:
+        raise ValueError("SCREEN_HEIGHT is None")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Pygame Window")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        black = (0, 0, 0)
+        screen.fill(black)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
