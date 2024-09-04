44517ad315a35d3ce7a3f6774af42c0bdd2e6990
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-04 09:14:52 -0300

    perf: game uses less resources

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pygame Window")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +24,8 @@ def main():
         black = (0, 0, 0)
         screen.fill(black)
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
 
 
 if __name__ == "__main__":
